f2a95f95a4f458cefe23afd1ca0cc331d111b0ab
s3688949 <83648757+s3688949@users.noreply.github.com>
2022-05-02 15:19:13 +1000

    Update App.js

--- source/samples/webclient/src/App.js
+++ source/samples/webclient/src/App.js
@@ -19,19 +19,31 @@ import React from 'react';
 import './App.css';
 import Banner from './components/Banner';
 import ChatSpace from './components/ChatSpace';
-import {AmplifyGreetings } from '@aws-amplify/ui-react';
+import Amplify from 'aws-amplify';
+import { AmplifyAuthenticator, AmplifyGreetings } from '@aws-amplify/ui-react';
+import {AuthState, onAuthUIStateChange} from "@aws-amplify/ui-components";
 
 declare var awsConfig;
 Amplify.configure(awsConfig);
 
 function App() {
-  return (
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
+  return authState === !AuthState.SignedIn && user ? (
     <div className="App">
-      <AmplifyGreetings username={"RMIT USER"} />
+      <AmplifyGreetings username={user.username} />
       <Banner />
       <ChatSpace />
     </div>
-  )
+  ) : (
+    <AmplifyAuthenticator />
+  );
 }
-
-export default App;
