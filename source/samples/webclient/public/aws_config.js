4ce930cd477c21a946532ea2afd46e17936a8432
s3688949 <83648757+s3688949@users.noreply.github.com>
2022-05-03 21:15:08 +1000

    Update aws_config.js

--- source/samples/webclient/public/aws_config.js
+++ source/samples/webclient/public/aws_config.js
@@ -41,7 +41,7 @@ var awsConfig = {
         }
       ]
     },
-    language: "EN_US",
+    language: "en_US",
     botVoice: "Joey",
     cognitoIdentityPool: "ap-southeast-2:c8c2a79d-ee46-404e-bb03-e0a5df41fa5",
     clientMetadata: { customUserAgent: "AwsSolution/SOL0027/1.6.0" },
