6e8201001c35389e9e4c784b9dcb3be6a9981e31
s3688949 <83648757+s3688949@users.noreply.github.com>
2022-05-08 14:41:22 +1000

    Attempt at adding text box 1

--- source/samples/webclient/src/components/ChatSpace.js
+++ source/samples/webclient/src/components/ChatSpace.js
@@ -15,7 +15,8 @@
  * @author Solution Builders
  */
 
-import React from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
+import Textarea from 'react-expanding-textarea'
 import { API, Auth } from 'aws-amplify';
 import { extractResponse, constructRequest, scrollToBottom, textToSpeech } from '../utils/utilityFunctions';
 declare var awsConfig;
@@ -72,6 +73,17 @@ class ChatSpace extends React.Component {
         scrollToBottom();
     }
 
+    const MyTextarea = () => {
+      const textareaRef = useRef(null)
+
+      const handleChange = useCallback(e => {
+        console.log('Changed value to: ', e.target.value)
+      }, [])
+
+      useEffect(() => {
+        textareaRef.current.focus()
+      }, [])
+
     render () {
         let {messages} = this.state;
         return (
@@ -80,6 +92,21 @@ class ChatSpace extends React.Component {
                     {this.showConversation(messages)}
                 </div>
                 <form className="textbox" id="chat-form" onSubmit={this.myFunction}>
+                    <>
+                      <label htmlFor="my-textarea">
+                        Please Enter Some Details:
+                      </label>
+                      <Textarea
+                        className="textarea"
+                        defaultValue="Lorem ipsum dolor sit amet, ..."
+                        id="my-textarea"
+                        maxLength="3000"
+                        name="pet[notes]"
+                        onChange={handleChange}
+                        placeholder="Enter additional notes..."
+                        ref={textareaRef}
+                      />
+                    </>
                     <input id='message' className="textbox--input" type='text' placeholder='Type a message... Try, help' ref={this.input}/>
                     <input type='submit' className="textbox--send" id="chat-send" value='Send'/>
                 </form>
